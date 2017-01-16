e554c7bd411c8669f94b223b598536f75974d24d
your github.com username <kandeel.a1@gmail.com>
2017-01-16 18:34:50 -0500

    exercise2.rb

--- exercise2.rb
+++ exercise2.rb
@@ -4,3 +4,10 @@ puts "tip is #{meal * 0.15}"
 puts 1.to_s + "this interpolation."
 
 puts " this is how you multiply #{45628 * 7839}. cool?"
+
+part 3: TRUE
+
+amount = 20
+new_amount = amount
+amount "twenty"
+first_amount, second_amount, third_amount
